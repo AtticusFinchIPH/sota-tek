3bc0c710c2eb10d263cea5df0b82b3cd78794228
AtticusFinchIPH <tranvan.duc.012@gmail.com>
2021-03-25 15:19:48 +0700

    Build components tree

---
+++ src/components/NewTask.js
@@ -0,0 +1,10 @@
+import './NewTask.css';
+
+const NewTask = () => {
+    return (
+        <div className="newTask container-md">
+            <h2>New Task</h2>
+        </div>
+    )
+}
+export default NewTask;
