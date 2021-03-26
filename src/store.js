035d37778ea5ae1851f6a5de8e663b44d907b516
AtticusFinchIPH <tranvan.duc.012@gmail.com>
2021-03-26 10:40:17 +0700

    Fix Redux debugging

--- src/store.js
+++ src/store.js
@@ -8,7 +8,7 @@ const reducer = combineReducers({
     tasks: tasksReducer
 });
 
-const composeEnhancer = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
 
 export default store;
