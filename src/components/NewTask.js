f3e51a83fc1cda7934031323a2f807ae86e761d3
AtticusFinchIPH <tranvan.duc.012@gmail.com>
2021-03-26 08:27:59 +0700

    Separate TaskForm from NewTask

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -1,89 +1,23 @@
 
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+
 import './NewTask.css';
-import { addTask } from '../actions/taskActions';
+import TaskForm from './TaskForm';
 
 const NewTask = () => {
-    const dispatch = useDispatch();
     const today = new Date();
     const date = `${today.getDate()}`.length === 1 ? `0${today.getDate()}` : `${today.getDate()}`;
     const month = `${today.getMonth()}`.length === 1 ? `0${today.getMonth()}` : `${today.getMonth()}`;
     const todayFormatted = `${today.getFullYear()}-${month}-${date}`;
-    const [title, setTitle] = useState("");
-    const [desc, setDesc] = useState("");
-    const [dueDate, setDueDate] = useState(todayFormatted);
-    const [priority, setPriority] = useState(2);
-    const changeTitle = (e) => {
-        e.preventDefault();
-        setTitle(e.target.value);
-    }
-    const changeDesc = (e) => {
-        e.preventDefault();
-        setDesc(e.target.value);
-    }
-    const changeDueDate = (e) => {
-        e.preventDefault();
-        console.log(e.target.value)
-        setDueDate(e.target.value);
-    }
-    const changePriority = (e) => {
-        e.preventDefault();
-        setPriority(e.target.value);
-    }
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        dispatch(addTask({ title, desc, dueDate, priority }));
+    const defaultTask = {
+        title: '',
+        desc: '',
+        dueDate: todayFormatted,
+        priority: 2
     }
     return (
         <div className="newTask container-md">
             <h2>New Task</h2>
-            <form className="newTask__form" onSubmit={handleSubmit}>
-                <input 
-                    className="newTask__title"
-                    type="text" required={true} placeholder="Add new task ..."
-                    name="nt__input__title"
-                    value={title}
-                    onChange={changeTitle}
-                />
-                <div className="newTask__desc flex-column">
-                    <label htmlFor="nt__textarea__desc"><b>Description</b></label>
-                    <textarea 
-                        name="nt__textarea__desc"
-                        rows="8"
-                        value={desc}
-                        onChange={changeDesc}
-                    />
-                </div>
-                <div className="newTask__date flex-column">
-                    <label htmlFor="nt__input__date"><b>Due Date</b></label>
-                    <input 
-                        type="date" placeholder="Add new task ..."
-                        name="nt__input__date"
-                        value={dueDate}
-                        onChange={changeDueDate}
-                    />
-                </div>
-                <div className="newTask__prio flex-column">
-                    <label htmlFor="nt__select__prio"><b>Priority</b></label>
-                    <select
-                        name="nt__select__prio"
-                        value={priority}
-                        onChange={changePriority}
-                        style={{ height: '40px'}}
-                    >
-                        <option value="1">High</option>
-                        <option value="2">Normal</option>
-                        <option value="3">Low</option>
-                    </select>
-                </div>
-                <button
-                    className="newTask__add"
-                    type="submit"
-                >
-                    Add
-                </button>
-            </form>
+            <TaskForm task={defaultTask} isNew={true} />
         </div>
     )
 }
---
+++ src/components/TaskForm.js
@@ -0,0 +1,96 @@
+
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import './NewTask.css';
+import './TaskForm.css';
+import { addTask } from '../actions/taskActions';
+
+const TaskForm = ({task, isNew}) => {
+    const dispatch = useDispatch();
+    const [title, setTitle] = useState(task.title);
+    const [desc, setDesc] = useState(task.desc);
+    const [dueDate, setDueDate] = useState(task.dueDate);
+    const [priority, setPriority] = useState(task.priority);
+    const changeTitle = (e) => {
+        e.preventDefault();
+        setTitle(e.target.value);
+    }
+    const changeDesc = (e) => {
+        e.preventDefault();
+        setDesc(e.target.value);
+    }
+    const changeDueDate = (e) => {
+        e.preventDefault();
+        console.log(e.target.value)
+        setDueDate(e.target.value);
+    }
+    const changePriority = (e) => {
+        e.preventDefault();
+        setPriority(e.target.value);
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(addTask({ title, desc, dueDate, priority }));
+    }
+    return (
+        <form className="taskForm" onSubmit={handleSubmit}>
+            <input 
+                className="taskForm__title"
+                type="text" required={true} placeholder="Add new task ..."
+                name="tf__input__title"
+                value={title}
+                onChange={changeTitle}
+            />
+            <div className="taskForm__desc flex-column">
+                <label htmlFor="tf__textarea__desc"><b>Description</b></label>
+                <textarea 
+                    name="tf__textarea__desc"
+                    rows="8"
+                    value={desc}
+                    onChange={changeDesc}
+                />
+            </div>
+            <div className="taskForm__date flex-column">
+                <label htmlFor="tf__input__date"><b>Due Date</b></label>
+                <input 
+                    type="date" placeholder="Add new task ..."
+                    name="tf__input__date"
+                    value={dueDate}
+                    onChange={changeDueDate}
+                />
+            </div>
+            <div className="taskForm__prio flex-column">
+                <label htmlFor="tf__select__prio"><b>Priority</b></label>
+                <select
+                    name="tf__select__prio"
+                    value={priority}
+                    onChange={changePriority}
+                    style={{ height: '40px'}}
+                >
+                    <option value="1">High</option>
+                    <option value="2">Normal</option>
+                    <option value="3">Low</option>
+                </select>
+            </div>
+            {
+                isNew
+                ?
+                <button
+                    className="taskForm__add"
+                    type="submit"
+                >
+                    Add
+                </button>
+                :
+                <button
+                    className="taskForm__update"
+                    type="submit"
+                >
+                    Update
+                </button>
+            }
+        </form>
+    )
+}
+
+export default TaskForm;
